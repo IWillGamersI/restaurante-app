558c1309dbc0b98d1ec887720410393cd3364cf9
Willian <gameloliniciante@gmail.com>
2025-10-11 19:21:44 +0100

    Atualizações

--- src/components/PWAInstallPrompt.tsx
+++ src/components/PWAInstallPrompt.tsx
@@ -102,7 +102,7 @@ export function PWAInstallPrompt() {
         <img src="/logo.png" alt="Logo" className="w-28 h-28 mb-2 animate-bounce rounded-full" />
 
         {/* Barra de progresso */}
-        {installing && (
+        {!installing && (
           <>
             <div className="w-full bg-gray-200 rounded-full h-4 overflow-hidden">
               <div
